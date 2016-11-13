781925ee2d287f35a34f5278b2628dfad4760290
Michal Kovacik <xkovacikm2@gmail.com>
2016-11-13 16:15:03 +0100

    seeds for production

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,6 @@ csv.each do |row|
   City.create name: row[1]
 end
 
-puts 'seeding users'
-10.times do
-  User.create email: Faker::Internet.email, name: Faker::Superhero.name, password: '123456', password_confirmation: '123456'
-end
-
 puts 'seeding categories'
 Category.create name: 'Hudba'
 Category.create name: 'Šport'
@@ -20,13 +15,3 @@ Category.create name: 'Jedlo'
 Category.create name: 'Kultúra'
 Category.create name: 'Iné'
 
-city = City.find_by name: 'Bratislava'
-users = User.all
-categories = Category.all
-puts 'seeding posts'
-20.times do
-  Post.create! name: Faker::StarWars.planet, description: Faker::Lorem.paragraphs(5),
-              time: Faker::Time.forward(200).to_i, entry_fee: rand(40), limited: rand < 0.5, limit: rand(15),
-              city: city, user: users[rand(users.size-1)+1], category: categories[rand(categories.size-1) +1],
-              locality: 'Mlynská dolina 53'
-end
